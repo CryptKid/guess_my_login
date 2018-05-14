36d06ff1ed7b82811956b5808faf74dd055361a7
CryptKid <CryptKid@users.noreply.github.com>
2018-05-14 08:43:37 +0200

    Added time calculation to how_easy_to_guess. Now you can find out time without having to bruteforce it

--- python2/how_easy_to_guess.py
+++ python2/how_easy_to_guess.py
@@ -1,14 +1,30 @@
 #!/usr/bin/python2
+
+import time
+#performing sample attack on 4 digits to find out pc speed
+start_time = time.time()
+btpin = 0
+while btpin != 9999:
+	btpin += 1
+#Calculating time per digit
+time_base = (time.time() - start_time)/10000
+#Asking for the number of digits
 print "How many digits does your pin have?",
 digits_string  = raw_input()
 digits = int(digits_string)
+#Doing some math to it
 after = 9 * pow(10,digits-1)
 before = 0
 for x in range(1, digits+1):
 	before = before + 9 * pow(10,x-1)
 percent = float(after) / float(before) * 100
-print "Before you told me, your pin has " + str(digits) + " digits, I would have needed a maximum of "
-print before
-print "attempts. Now I do only need up to "
-print after
-print "attempts. That's only " + str(percent) + "% of the afford"
+#Telling you the results
+print("Before you told me, your pin has " + str(digits) + " digits, I would have needed a maximum of ")
+print(before)
+print("attempts. I would have needed")
+print(str(float(time_base * pow(10, digits))/percent*100))
+print("seconds to perform an attack. Now I would only need ")
+print (after)
+print ("attempts. I can perform them in")
+print (str(time_base * pow(10, digits)))
+print ("seconds. That's only " + str(percent) + "% of the afford")
--- python3/how_easy_to_guess.py
+++ python3/how_easy_to_guess.py
@@ -1,15 +1,30 @@
 #!/usr/bin/python3
 
+import time
+#asking for amount of digits
 print("How many digits does your pin have?")
 digits_string  = input()
+start_time = time.time()
+#performing a sample attack on a 4 digits pin to test pc speed
+btpin = 0
+while btpin != 9999:
+	btpin += 1
+#calculating time per digit
+time_base = (time.time() - start_time)/10000
+#adding some math
 digits = int(digits_string)
 after = 9 * pow(10,digits-1)
 before = 0
 for x in range(1, digits+1):
 	before = before + 9 * pow(10,x-1)
 percent = float(after) / float(before) * 100
+#tell you the results
 print("Before you told me, your pin has " + str(digits) + " digits, I would have needed a maximum of ")
 print(before)
-print("attempts. Now I do only need up to ")
+print("attempts. I would have needed")
+print(str(float(time_base * pow(10, digits))/percent*100))
+print("seconds to perform an attack. Now I would only need ")
 print (after)
-print ("attempts. That's only " + str(percent) + "% of the afford")
+print ("attempts. I can perform them in")
+print (str(time_base * pow(10, digits)))
+print ("seconds. That's only " + str(percent) + "% of the afford")
